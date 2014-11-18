edc280fea1b888496cce7600bb0cab74cfc185cb
Dana Mulder <danarmulder@gmail.com>
2014-11-18 11:43:50 -0800

    Finish homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
 class PagesController < ApplicationController
 
+  def verse_2
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+  get "verse-2" => 'pages#verse_2'
 end
